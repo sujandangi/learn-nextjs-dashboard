b4628b19f99b25d2e640c63718b3134330b2b283
Sujan Singh Dangi <sujandangi@gmail.com>
2024-01-24 15:57:27 +0530

    Project Complete. Committing after running npm run build and the build is working in local env as expected.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# Learning Outcome
+
+This was a fun hobby project for me to understand and grasp the concepts of Next.js framework.
+
+![Screenshot describing my learning by following along the next.js tutorial](./public/next-learnings.png)
+
+Login credentials
+username: user@nextmail.com
+password: 123456
+
 ## Next.js App Router Course - Starter
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
+import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
@@ -13,8 +13,10 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape}></div> 
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
+          <div className={styles.shape}></div>
+          <p
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-300">
               Next.js Learn Course
@@ -31,11 +33,11 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src='/hero-desktop.png'
-            width= {1000}
+            src="/hero-desktop.png"
+            width={1000}
             height={760}
-            className='hidden md:block'
-            alt='Screenshots of dashboard project'
+            className="hidden md:block"
+            alt="Screenshots of dashboard project"
           />
         </div>
       </div>
